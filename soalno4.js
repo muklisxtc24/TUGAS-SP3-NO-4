2871e6ec5ae8d1bb7c4a6e05fc7f5c099b420ca6
muklisxtc24 <87222847+muklisxtc24@users.noreply.github.com>
2021-08-03 07:44:15 +0700

    Add files via upload

---
+++ soalno4.js
@@ -0,0 +1,6 @@
+const person = ['rizal','prabumulih','prabujaya','sukajadi'];
+        const [nama,asal,rest]= prabumulih =person;
+        console.log(nama);
+        console.log(asal);
+        console.log(rest);
+        
